039d8e375a9a4682a1f3fa2b754bd92cd317e292
batuhan <batusalkim11@gmail.com>
2025-02-19 23:15:31 +0300

    fake pdfler eklendi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 'use client';
 
-import type { Metadata } from "next";
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { useEffect, useState } from 'react';
+import { metadata } from './metadata';
 
 const inter = Inter({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-  title: "Hukuk Notları",
-  description: "Hukuk öğrencileri için not paylaşım platformu",
-};
 
 export default function RootLayout({
   children,
---
+++ src/app/metadata.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Hukuk Notları",
+  description: "Hukuk öğrencileri için not paylaşım platformu",
+};
